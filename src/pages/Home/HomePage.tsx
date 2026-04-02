bfe24b1f2c2493d2067c27d0166591099cf8b8e8
Serhii Kozhyn <serginho915@gmail.com>
2026-04-02 12:26:42 +0300

    titles

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./HomePage.module.scss";
 import { Hero } from "@/sections/Hero/Hero";
+import { Menu } from "@/sections/Menu/Menu";
 
 export default function HomePage() {
   return (
     <div className={styles.page}>
       <Hero />
+      <Menu />
     </div>
   );
 }
---
+++ src/sections/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+import styles from './Menu.module.scss'
+
+export const Menu = () => {
+  return (
+    <section>
+        <h1 className={styles.mainTitle}>Днешно Меню</h1>
+        <h2 className={styles.subTitle}>Пссст… още е горещо!</h2>
+    </section>
+  )
+}
