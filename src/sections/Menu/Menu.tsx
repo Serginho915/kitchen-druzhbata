bdc34f155d27e8b64527fe82ad28ddcf7b0fc11a
Serhii Kozhyn <serginho915@gmail.com>
2026-04-14 15:34:56 +0300

    added animation menu

--- src/sections/Menu/Menu.tsx
+++ src/sections/Menu/Menu.tsx
@@ -40,7 +40,7 @@ export const Menu: React.FC<MenuProps> = ({ data }) => {
               />
             </div>
             <h2 className={styles.title}>{category.title}</h2>
-            <div className={styles.arrow}></div>
+            <div className={`${styles.arrow} ${openCategoryIds.includes(category.id) ? styles.arrowActive : ""}`}></div>
           </div>
 
           <div
