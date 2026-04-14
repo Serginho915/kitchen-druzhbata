3a54808ef5a146d86dbc4698d6914d2e4ac4ff5c
Serhii Kozhyn <serginho915@gmail.com>
2026-04-14 17:26:59 +0300

    making dropdown mob

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -8,7 +8,7 @@ import phone from "@/assets/images/Vectors/phone.svg";
 import burger from "@/assets/images/Vectors/burger.svg";
 import Link from "next/link";
 import logo from "@/assets/images/logo.png";
-import navigate from '@/assets/images/Vectors/navigate.svg'
+import navigate from "@/assets/images/Vectors/navigate.svg";
 import { useCart } from "@/hooks/useCart";
 
 export const Header = () => {
@@ -63,16 +63,18 @@ export const Header = () => {
   }, [isMenuOpen]);
 
   return (
-    <header className={`${styles.header} ${!isVisible ? styles.headerHidden : ""}`}>
+    <header
+      className={`${styles.header} ${!isVisible ? styles.headerHidden : ""}`}
+    >
       <div className={styles.logoBlock}>
         <Link href="/">
           <Image
-           src={logo} 
-           alt="Kitchen Druzhbata Logo"  
-           className={styles.logo} 
-           width={80}
-           height={80}
-           />
+            src={logo}
+            alt="Kitchen Druzhbata Logo"
+            className={styles.logo}
+            width={80}
+            height={80}
+          />
         </Link>
       </div>
 
@@ -94,18 +96,32 @@ export const Header = () => {
       </nav>
 
       <div className={styles.userBlock}>
-        <div 
+        <div
           className={`${styles.userItem} ${styles.bgUserBlock} ${styles.cartBlock}`}
           onClick={toggleCart}
         >
           {cartCount > 0 && (
-            <span className={styles.cartBadge}>{cartCount > 99 ? "99+" : cartCount}</span>
+            <span className={styles.cartBadge}>
+              {cartCount > 99 ? "99+" : cartCount}
+            </span>
           )}
-          <Image src={cart} alt="Cart Icon" width={64} height={64} className={styles.icon} />
+          <Image
+            src={cart}
+            alt="Cart Icon"
+            width={64}
+            height={64}
+            className={styles.icon}
+          />
         </div>
 
         <div className={`${styles.phoneIcon} ${styles.bgUserBlock}`}>
-          <Image src={phone} alt="Phone Icon" width={64} height={64} className={styles.icon} />
+          <Image
+            src={phone}
+            alt="Phone Icon"
+            width={64}
+            height={64}
+            className={styles.icon}
+          />
         </div>
 
         <div
@@ -113,7 +129,13 @@ export const Header = () => {
           className={`${styles.burgerIcon} ${styles.bgUserBlock} ${isMenuOpen ? styles.burgerActive : ""}`}
           onClick={toggleMenu}
         >
-          <Image src={burger} alt="Burger Menu" width={64} height={64} className={styles.icon} />
+          <Image
+            src={burger}
+            alt="Burger Menu"
+            width={64}
+            height={64}
+            className={styles.icon}
+          />
         </div>
       </div>
 
@@ -133,31 +155,26 @@ export const Header = () => {
               <Link href="/subscriptions">Подписки</Link>
             </li>
           </ul>
-          <div className={styles.mobileInfo}>
-              <div>
-              <Link
-                href={'phone:+359 8899999'}
-              >
-                <Image
-                src={phone}
-                alt="call"
-                >
-                </Image>
-              </Link>
-            </div>
-            <div className={styles.mobileInfo}>
-              <Link
-                href={'https://maps.app.goo.gl/4W58KxWc4GTXwJPRA'}
-              >
-                <Image
-                src={navigate}
-                alt="navigate"
-                >
-                </Image>
-              </Link>
 
-            </div>
-          </div>
+          <ul className={styles.mobileInfo}>
+            <li className={styles.mobileItem}>
+              <div className={styles.linkWrapper}>
+                <p className={styles.mobileInfoText}>Контакт</p>
+                <Link href={"phone:+359 8899999"}>
+                  <Image src={phone} alt="call"></Image>
+                </Link>
+              </div>
+            </li>
+
+            <li className={styles.mobileItem}>
+              <p className={styles.mobileInfoText}>Контакт</p>
+              <div className={styles.linkWrapper}>
+                <Link href={"https://maps.app.goo.gl/4W58KxWc4GTXwJPRA"}>
+                  <Image src={navigate} alt="navigate"></Image>
+                </Link>
+              </div>
+            </li>
+          </ul>
         </div>
       )}
     </header>
