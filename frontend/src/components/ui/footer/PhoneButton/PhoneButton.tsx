d4425e696551f0291cbf4223a692805c260599e5
Serhii Kozhyn <serginho915@gmail.com>
2026-04-24 10:30:27 +0100

    prevented default phone link

--- frontend/src/components/ui/footer/PhoneButton/PhoneButton.tsx
+++ frontend/src/components/ui/footer/PhoneButton/PhoneButton.tsx
@@ -37,8 +37,8 @@ export const PhoneButton: React.FC<PhoneButtonProps> = ({ className }) => {
 
   const handlePhoneClick = (e: React.MouseEvent) => {
     if (!isMobile) {
+      e.preventDefault();
       if (!isPhoneExpanded) {
-        e.preventDefault();
         setIsPhoneExpanded(true);
       }
     }
